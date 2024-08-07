ea72b4afa1380b3d16cbac888b06eaca2153d8b9
Melvin-Emmanuel <emmanulmelv@gmail.com>
2024-08-07 14:33:11 +0100

    connected to atlas 2

--- dist/database/database.js
+++ dist/database/database.js
@@ -5,7 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const localUrl = "mongodb://0.0.0.0:27017/Melasi-stores";
-const db = mongoose_1.default.connect(localUrl).then(() => {
+const onlineUrl = "mongodb+srv://emmanulmelv:0n4DIP5zo5tLVmn3@cluster0.r6gtk6f.mongodb.net/";
+const db = mongoose_1.default.connect(onlineUrl).then(() => {
     console.log("A connection has been made");
 }).catch((error) => {
     console.log(error, "there was an error in database connection");
