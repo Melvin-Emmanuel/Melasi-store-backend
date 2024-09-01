39d91151bfd2946afa8c12959dbf5cb5a47f5497
Melvin-Emmanuel <emmanulmelv@gmail.com>
2024-09-01 17:55:12 +0100

    fixed tomen issue

--- Router/CategoryRouter.ts
+++ Router/CategoryRouter.ts
@@ -1,11 +1,16 @@
 import express from "express";
-import { verifyToken } from "../Middleware/verify"
-import { CreateCategory, updateCategory, DeletCategory, GetAllCategory} from "../controller/CategoryController";
+import { verifyToken } from "../Middleware/verify";
+import {
+  CreateCategory,
+  updateCategory,
+  DeletCategory,
+  GetAllCategory,
+} from "../controller/CategoryController";
 
 const router = express.Router();
-router.route("/create-Category").post(CreateCategory);
+router.route("/create-Category").post(verifyToken,CreateCategory);
 // router.route("/get-All-user").post(CreateCategory);
-router.route("Update-Category").post(verifyToken, updateCategory)
-router.route("Delete-Category").post(verifyToken, DeletCategory)
-router.route("Get-All-Categories").get(verifyToken,GetAllCategory)
+router.route("Update-Category").post(verifyToken, updateCategory);
+router.route("Delete-Category").post(verifyToken, DeletCategory);
+router.route("Get-All-Categories").get(verifyToken, GetAllCategory);
 export default router;
